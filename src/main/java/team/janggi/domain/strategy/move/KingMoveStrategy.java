79f3983f6eaffbaf267a840d7093f39250963b5a
wonsang.choi <wonsang.choi@e4net.net>
2026-03-27 17:12:52 +0900

    feat: 궁 움직임 구현

--- src/main/java/team/janggi/domain/strategy/move/KingMoveStrategy.java
+++ src/main/java/team/janggi/domain/strategy/move/KingMoveStrategy.java
@@ -11,7 +11,20 @@ public class KingMoveStrategy implements MoveStrategy {
 
     @Override
     public boolean calculateMove(Position from, Position to, Map<Position, Piece> mapStatus) {
-        return false;
+        return validateDirection(from, to) && !validateObstacle(from, to, mapStatus);
+    }
+
+    private boolean validateDirection(Position from, Position to) {
+        int dx = Math.abs(from.x() - to.x());
+        int dy = Math.abs(from.y() - to.y());
+
+        return (dx + dy) == MAX_MOVE_DISTANCE || (dx == MAX_MOVE_DISTANCE) && (dy == MAX_MOVE_DISTANCE);
+    }
+
+    private boolean validateObstacle(Position from, Position to, Map<Position, Piece> mapStatus) {
+        Piece toPiece = mapStatus.get(to);
+        Piece fromPiece = mapStatus.get(from);
+        return toPiece.isSameTeam(fromPiece);
     }
 
 }
